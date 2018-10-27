4efa8f6be77f565fc5bd5fa3525cafca74b056c6
Nguyen Le <ucnguyenle@gmail.com>
2018-10-27 11:21:24 -0700

    made changes to apiroutes.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,7 +7,7 @@
 var friendsData = [
     {
         "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://images.axios.com/078ps5QcbNgqsgEI_QkV9hDW9PQ=/0x0:3900x2194/1920x1080/2018/08/25/1535212462538.jpg",
         "scores":[
             5,
             1,
@@ -20,7 +20,88 @@ var friendsData = [
             4,
             1
           ]
+      },
+      {
+        "name":"Win",
+        "photo":"https://images.axios.com/078ps5QcbNgqsgEI_QkV9hDW9PQ=/0x0:3900x2194/1920x1080/2018/08/25/1535212462538.jpg",
+        "scores":[
+            1,
+            2,
+            3,
+            3,
+            4,
+            5,
+            5,
+            3,
+            4,
+            1
+          ]
+      },
+      {
+        "name":"Jon",
+        "photo":"https://images.axios.com/078ps5QcbNgqsgEI_QkV9hDW9PQ=/0x0:3900x2194/1920x1080/2018/08/25/1535212462538.jpg",
+        "scores":[
+            4,
+            3,
+            2,
+            1,
+            4,
+            3,
+            2,
+            4,
+            5,
+            1
+          ]
+      },
+      {
+        "name":"Christine",
+        "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUtlB3PewteN8QS2OWqJTWob_KKZTQS4UX779ACXcvPPh9rAREpg",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+          ]
+      },
+      {
+        "name":"Vincent",
+        "photo":"https://hearcolorado.org/wp-content/uploads/2016/07/famous-peoples-hearing-loss.jpg",
+        "scores":[
+            2,
+            2,
+            5,
+            5,
+            5,
+            1,
+            5,
+            5,
+            4,
+            5
+          ]
+      },
+      {
+        "name":"Sarah",
+        "photo":"https://peopleprogram.wiscweb.wisc.edu/wp-content/uploads/sites/118/2017/06/Maichou-Lor-600x400.jpg",
+        "scores":[
+            5,
+            2,
+            1,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
       }
+
 
   ];
   
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -19,72 +19,37 @@ module.exports = function(app) {
   // API POST Requests
   // ---------------------------------------------------------------------------
 
-  app.post("/api/friends", function(req, res) {
-      var match = {
-        name: "",
-        photo: "",
-        difference: 1000
-      };
+  app.post('/api/friends', function(req,res){
+    //grabs the new friend's scores to compare with friends in friendList array
+    var newFriendScores = req.body.scores;
+    var theScores = [];
+    var bestMatch = 0;
 
-      // need logic to do the following
-        // Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
-        // With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
+    //runs through all current friends in list
+    for(var i=0; i < friendsData.length; i++){
+      var theDiff = 0;
+      //run through scores to compare friends
+      for(var j=0; j<newFriendScores.length; j++){
+        theDiff += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(newFriendScores[j])));
+      }
 
+      //push results into scores array
+      theScores.push(theDiff);
+    }
 
-        // Example: 
+    //after the friends are compared, find best match
+    for(var i = 0; i < theScores.length; i++){
+        var score = theScores[i];
+      if(score <= theScores[bestMatch]){
+        bestMatch = i;
+      }
+    }
 
+    //return bestMatch data
+    var match = friendsData[bestMatch];
+    res.json(match);
 
-        // User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-
-        // User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
-
-        // Total Difference: 2 + 1 + 2 = 5
-
-
-            // Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on. 
-            // The closest match will be the user with the least amount of difference.
-
-
-
-            // Once you've found the current user's most compatible friend, display the result as a modal pop-up.
-
-
-            // The modal should display both the name and picture of the closest match.
-
-        console.log(req.body);
-
-        var user = req.body;
-        var score = user.scores;
-        var theDifference;
-
-        // loop through friendsData
-        for (var i = 0; i < friendsData.length; i++) {
-            // set a variable for each friendsData
-            var friendData = friendsData[i];
-            console.log(friendData);
-
-            // will be used to calculate difference
-            theDifference = 0;
-
-            // loop through the scores of the users
-            for (var j = 0; j < score.length; j++) {
-                // set each score to a variable
-                var eachScore = score[j];
-                // take each score and subtract it from friendData score
-                theDifference += Math.abs(eachScore - friendData.eachScore);
-
-            if (match.difference >= theDifference) {
-                match.name = friendData.name;
-                match.photo = friendData.photo;
-                match.difference = theDifference;
-                }
-            }
-        }
-
-        friendsData.push(user);
-        res.json(match);
-
-    });
-  };
-
-  
+    //pushes new submission into the friendsList array
+    friendsData.push(req.body);
+  });
+};
